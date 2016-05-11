0e49a38bf92462e22e36fb13d31f45c98cd5ee2e
Bruno Magrini <brmagrini@gmail.com>
2016-05-10 21:53:55 -0300

    comeco das correcoes

--- alm_templates/default.php
+++ alm_templates/default.php
@@ -10,12 +10,12 @@
 									// 	<h1>".get_the_title( )."</h1>
 						 		// 	</a>
 						 		// </article>";
-						 		$paginas .="
+						 		
+						 		echo "
 						 		<article>
 						 			<a href='".get_permalink( )."'>
 						 				<h1>".get_the_title( )."</h1>
 						 			</a>
 						 		</article>";
-						 		echo $paginas;
 						 	}
 						  ?>
